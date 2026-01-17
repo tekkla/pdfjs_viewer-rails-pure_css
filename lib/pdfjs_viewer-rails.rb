1f4b379b72055d8c06abbe7a9c646e38cec201d0
Tekkla <m.zorn@tekkla.de>
2026-01-17 20:36:58 +0100

    Add asset path configuration for `pdfjs_viewer` and update asset links.
    - Configured Rails to include `pdfjs_viewer` in the asset pipeline during initialization.
    - Adjusted asset paths in the viewer's `_head.html.erb` for better compatibility.

--- lib/pdfjs_viewer-rails.rb
+++ lib/pdfjs_viewer-rails.rb
@@ -6,6 +6,10 @@ module PdfjsViewer
     class Engine < ::Rails::Engine
       isolate_namespace PdfjsViewer
 
+      initializer "pdfjs_viewer-rails.assets" do |app|
+        app.config.assets.paths << root.join("app/assets/pdfjs_viewer")
+      end
+
       initializer "pdfjs_viewer-rails.view_helpers" do
         ActionView::Base.send :include, ViewHelpers
       end
